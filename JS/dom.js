c0008d6471bb353a25801334689f321ac0bcef59
Harshitabes2006 <singhharshit0606@gmail.com>
2026-03-19 08:13:35 +0530

    js

---
+++ JS/dom.js
@@ -0,0 +1,10 @@
+function result() {
+    document.getElementById("text").style.color = "beige";
+    document.getElementsByTagName('body')[0].style.backgroundColor = "black";
+    document.getElementById("text").style.textDecoration = "underline";
+    document.getElementById("text").style.textAlign = "center";
+     document.getElementById("para").style.color="yellow";
+    document.getElementById("para").style.textAlign = "center";
+}
+
+dom.js
